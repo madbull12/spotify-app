3ffef6b719a0d4e69f98e4c0e367f30a779a7429
madbull12 <huangandrian02@gmail.com>
2022-05-26 11:52:35 +0700

    bug fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
 import { RecoilRoot } from 'recoil'
 
 
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+
   return (
     <SessionProvider session={session}>
       <RecoilRoot>
