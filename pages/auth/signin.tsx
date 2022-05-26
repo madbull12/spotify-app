a9bf6edef6ea24ddbc3d467cf3e8fe367d9fd997
madbull12 <huangandrian02@gmail.com>
2022-05-26 11:12:56 +0700

    bug fixed

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -31,7 +31,7 @@ const SignInPage:NextPage = ({ providers }:any) => {
             <title>Login</title>
         </Head>
         <Image src={logo} width={600} height={250} alt="logo" objectFit='contain' className='animate-pulse' />
-        {Object.values(providers).map((provider) => (
+        {Object.values(providers).map((provider:any) => (
             <div key={provider.name}>
                 <button onClick={() => signIn(provider.id)} className="text-white font-bold text-xl bg-green-500 px-6 py-4 rounded-md hover:bg-green-600 transition-all ease-in-out duration-200 " >
                     Sign in with {provider.name}
